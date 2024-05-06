6579fd07b85350fc96438ed371f818dfde9521fe
Abel Ingrand <abel@ingrand.net>
2024-05-06 11:54:26 +0200

    Ajout d'une option pour cocher automatiquemet la case \'Je suis le médecin traitant\' si c'est le cas

--- options.js
+++ options.js
@@ -33,6 +33,7 @@ document.addEventListener('DOMContentLoaded', function () {
     'KeyPadPrescription': true,
     'TweakFSEGestion': true,
     'TweakFSECreation': true,
+    'TweakFSEDetectMT' : false,
     'autoSelectPatientCV': true,
     'WarpButtons': true,
     'autoConsentNumPres': false,
