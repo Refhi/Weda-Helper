aa326a5224dbc2e5e17200ae58c46e50c8f8df48
Refhi <wedahelper@mailo.com>
2024-09-15 22:43:15 +0200

    avancées sur le refactory de print.js (jsDoc & co)

--- print2.js
+++ print2.js
@@ -1,94 +1,179 @@
-// Fonction permettant d'imprimer selon les options choisies
-function printIfOption(modelNumber = 0) {
-    getOption('RemoveLocalCompanionPrint', function (RemoveLocalCompanionPrint) {
-        if (!RemoveLocalCompanionPrint) {
-            startPrinting('companion', modelNumber);
-        } else {
-            startPrinting('print', modelNumber);
-        }
+// Refactory (encore) de print.js
+
+// Flux principal :
+// On part de la demande d'impression suite à ctrl+P, et de là, on doit déterminer le reste.
+
+/**
+ * Fonction d'entrée pour l'impression ou le téléchargement.
+ * Les arguments dépendent du raccourci clavier utilisé :
+ * - `ctrl+P` => Impression, si pertinent du modèle 0
+ * - `ctrl+shift+P` => Impression, si pertinent du modèle 1
+ * - `ctrl+D` => Téléchargement, si pertinent du modèle 0
+ * - `ctrl+shift+D` => Téléchargement, si pertinent du modèle 1
+ *
+ * @param {string} printType - Le type d'action à effectuer, soit "print" pour impression, soit "download" pour téléchargement.
+ * @param {number} [modelNumber=0] - Le numéro du modèle à utiliser, par défaut 0.
+ *
+ * @example
+ * // Impression du modèle 0 (si pertinent)
+ * print('print', 0);
+ *
+ * @example
+ * // Téléchargement du modèle 1 (si pertinent)
+ * print('download', 1);
+ */
+function print(printType, modelNumber = 0) {
+    // D'abord récupération de l'ensemble des options pertinentes dans les options.
+    // Pour l'instant, juste RemoveLocalCompanionPrint
+    getOption(['RemoveLocalCompanionPrint'], function (RemoveLocalCompanionPrint) {
+        // De quel type d'"impression" s'agit-il ? (impression, téléchargement, compagnon)
+        const handlingType = deduceHandlingType(printType, RemoveLocalCompanionPrint);
+        // Qu'imprime-t-on ? (courbe, FSE ou modèle)
+        const whatToPrint = deduceWhatToPrint();
+
+        // On lance le processus d'impression
+        startPrinting(handlingType, whatToPrint, modelNumber);
     });
 }
 
-// Définition de la fonction startPrinting
-function startPrinting(handlingType, modelNumber = null) {
-    // TODO :
-    // - sortir les sous-fonctions de startPrinting
-    // - renommer en printkkchose
+/**
+ * Détermine le type de demande d'impression ou de téléchargement.
+ */
+function deduceHandlingType(printType, RemoveLocalCompanionPrint) {
+    if (printType === 'print') {
+        return RemoveLocalCompanionPrint ? 'print' : 'companion';
+    } else if (printType === 'download') {
+        return 'download';
+    } else {
+        throw new Error('Type de demande non valide');
+    }
+}
+
+/**
+ * Déduit le type de contenu à imprimer ou télécharger en fonction de l'URL actuelle.
+ */
+function deduceWhatToPrint() {
+    // Dictionnaire des scénarios
+    const scenarios = {
+        'courbe': () => window.location.href.startsWith('https://secure.weda.fr/FolderMedical/ConsultationForm.aspx'),
+        'fse': () => window.location.href.startsWith('https://secure.weda.fr/vitalzen/fse.aspx'),
+        'model': () => true // Par défaut, si aucun autre scénario ne correspond
+    };
+
+    // Itérer sur les scénarios pour trouver celui qui correspond
+    for (const [scenario, condition] of Object.entries(scenarios)) {
+        if (condition()) {
+            return scenario;
+        }
+    }
+
+    // Si aucun scénario ne correspond, retourner null
+    return null;
+}
+
+/**
+ * Récupère l'URL du PDF à partir des données présentes dans l'image.
+ * Utile pour récupérer le PDF d'une courbe.
+ *
+ * @returns {string|null} - L'URL du PDF ou null si non détecté.
+ */
+function fetchPdfUrlFromImageData() {
+    var pdfUrl = document.querySelector('img[data-pdf-url]');
+    if (pdfUrl) {
+        console.log('[fetchPdfUrlFromImageData] pdf Url détecté :', pdfUrl);
+        let url = pdfUrl.getAttribute('data-pdf-url');
+        return url;
+    } else {
+        console.log('[fetchPdfUrlFromImageData] pdfUrl non détecté');
+        return null;
+    }
+}
+
+
+/**
+ * Crée et ajoute un iframe caché pour faciliter l'impression.
+ *
+ * @returns {HTMLIFrameElement} - L'iframe caché créé.
+ */
+function createEmptyHiddenIframe() {
+    // Crée un nouvel élément iframe pour l'impression
+    let hiddenFrame = document.createElement('iframe');
+    hiddenFrame.name = 'print_frame';
+    hiddenFrame.width = '0';
+    hiddenFrame.height = '0';
+    hiddenFrame.style.display = 'none';
+    document.body.appendChild(hiddenFrame);
+    return hiddenFrame;
+}
+
+/**
+ * Télécharge un blob à partir d'une URL.
+ *
+ * @param {string} url - L'URL du fichier à télécharger.
+ * @returns {Promise<Blob|null>} - Une promesse qui se résout avec le blob téléchargé ou null en cas d'erreur.
+ */
+async function fetchBlobFromUrl(url) {
+    console.log('fetchPDF', url);
+    try {
+        const response = await fetch(url);
+        const blob = await response.blob();
+        return blob;
+    } catch (error) {
+        console.error('Error:', error);
+        return null;
+    }
+}
+
+/**
+ * Charge une URL dans un iframe et affiche la boîte de dialogue d'impression lorsque l'iframe est chargée.
+ *
+ * @param {HTMLIFrameElement} iframe - L'iframe dans laquelle charger l'URL. Normalement énérée par `createEmptyHiddenIframe`.
+ * @param {string} url - L'URL du pdf à charger dans l'iframe.
+ */
+function loadUrlAndShowPrintDialog(iframe, url) {
+    // Définit une fonction à exécuter lorsque l'iframe est chargée
+    iframe.onload = function () {
+        let win = window.frames['print_frame'];
+        win.focus();
+        win.print();
+    };
+
+    // Vérifie l'origine de l'URL
+    let urlObject = new URL(url);
+    if (urlObject.origin === 'https://secure.weda.fr') {
+        console.log('url origin ok', urlObject.origin);
+        iframe.src = url; // C'est ici que l'URL est chargée
+    } else {
+        // Log en cas d'URL non fiable
+        console.error('Untrusted URL:', url);
+    }
+}
+
+/**
+ * Force le téléchargement direct d'un fichier à partir d'une URL.
+ *
+ * @param {string} url - L'URL du fichier à télécharger.
+ */
+function triggerDirectDownload(url) {
+    // On va contourner les restrictions de téléchargement en créant un élément 'a' caché
+    // Ce dernier, quand cliqué, va déclencher le téléchargement du fichier via son attribut 'download'
+    // Cela permet de télécharger le fichier sans modifier le manifest
+    var link = document.createElement('a');
+    link.href = url;
+    link.download = ''; // Pour forcer le téléchargement
+    link.style.display = 'none';
+    document.body.appendChild(link);
+    link.click(); // Cela déclenche le téléchargement
+    document.body.removeChild(link); // Suppression de l'élément 'a' après le téléchargement
+}
+
+
+///////////////////// OLD ///////////////////////
+
+function startPrinting(handlingType, whatToPrint, modelNumber) { // TODO : à renommer en ?
     console.log('startPrinting activé');
-    let courbe = window.location.href.startsWith(
-        'https://secure.weda.fr/FolderMedical/ConsultationForm.aspx'
-    );
-    let isFSE = window.location.href.startsWith('https://secure.weda.fr/vitalzen/fse.aspx');
-    let whatToPrint = courbe ? 'courbe' : (isFSE ? 'fse' : modelNumber);
+
     processPrintSequence(handlingType, whatToPrint);
-
-    function urlFromImage() { // à renommer, par exemple fetchPdfUrlFromImageData
-        var pdfUrl = document.querySelector('img[data-pdf-url]');
-        if (pdfUrl) {
-            console.log('[urlFromImage] pdf Url détecté :', pdfUrl);
-            let url = pdfUrl.getAttribute('data-pdf-url');
-            return url;
-        } else {
-            console.log('[urlFromImage] pdfUrl non détecté');
-            return null;
-        }
-    }
-
-    function makeIframe() { // à renommer makeHiddenIframe
-        // Crée un nouvel élément iframe pour l'impression
-        let printFrame = document.createElement('iframe');
-        printFrame.name = 'print_frame';
-        printFrame.width = '0';
-        printFrame.height = '0';
-        printFrame.style.display = 'none';
-        document.body.appendChild(printFrame);
-        return printFrame;
-    }
-
-    async function downloadBlob(url) {
-        console.log('fetchPDF', url);
-        try {
-            const response = await fetch(url);
-            const blob = await response.blob();
-            return blob;
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    }
-
-
-    function loadAndPrintIframe(iframe, url) {
-        // Définit une fonction à exécuter lorsque l'iframe est chargée
-        iframe.onload = function () {
-            let win = window.frames['print_frame'];
-            win.focus();
-            win.print();
-        };
-
-        // Vérifie l'origine de l'URL
-        let urlObject = new URL(url);
-        if (urlObject.origin === 'https://secure.weda.fr') {
-            console.log('url origin ok', urlObject.origin);
-            iframe.src = url;
-        } else {
-            // Log en cas d'URL non fiable
-            console.error('Untrusted URL:', url);
-        }
-    }
-
-    // déclenche le téléchargement direct d'une url
-    function download(url) {
-        // On va contourner les restrictions de téléchargement en créant un élément 'a' caché
-        // Ce dernier, quand cliqué, va déclencher le téléchargement du fichier via son attribut 'download'
-        // Cela permet de télécharger le fichier sans modifier le manifest
-        var link = document.createElement('a');
-        link.href = url;
-        link.download = ''; // Pour forcer le téléchargement
-        link.style.display = 'none';
-        document.body.appendChild(link);
-        link.click(); // Cela déclenche le téléchargement
-        document.body.removeChild(link); // Suppression de l'élément 'a' après le téléchargement
-    }
 
     function clickPrinterNumber(modelNumber = 0) { // TODO -> clickPrintModelNumber
         var elements = document.querySelectorAll('[onclick*="ctl00$ContentPlaceHolder1$MenuPrint"][class*="popout-dynamic level2"]');
@@ -194,19 +279,19 @@ function startPrinting(handlingType, modelNumber = null) {
 
         // deux grands cas de figure : impression d'une courbe ou d'un document
         if (whatToPrint === 'courbe') {
-            let url = urlFromImage();
+            let url = fetchPdfUrlFromImageData();
             if (!url) {
                 console.log('[processPrintSequence] URL non trouvée');
                 return;
             }
             if (handlingType === 'print') {
-                let iframe = makeIframe();
-                loadAndPrintIframe(iframe, url);
+                let iframe = createEmptyHiddenIframe();
+                loadUrlAndShowPrintDialog(iframe, url);
             } else if (handlingType === 'companion') {
-                downloadBlob(url)
+                fetchBlobFromUrl(url)
                     .then(blob => { sendToCompanion('print', blob); });
             } else if (handlingType === 'download') {
-                download(url);
+                triggerDirectDownload(url);
             }
         } else if (whatToPrint === 'fse') {
             console.log('printing FSE');
@@ -265,13 +350,13 @@ function startPrinting(handlingType, modelNumber = null) {
             })
             .then(url => {
                 if (handlingType === 'companion') {
-                    downloadBlob(url)
+                    fetchBlobFromUrl(url)
                         .then(blob => {
                             sendToCompanion('print', blob,
                                 postPrintAction);
                         });
                 } else if (handlingType === 'download') {
-                    download(url);
+                    triggerDirectDownload(url);
                     postPrintAction();
                 }
             });
