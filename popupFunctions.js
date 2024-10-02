8d21dcc86e17ffc0a2b292ac4960be116aab5c05
Refhi <wedahelper@mailo.com>
2024-10-02 18:03:16 +0200

    bugfixes suite scissions

--- popupFunctions.js
+++ popupFunctions.js
@@ -11,3 +11,45 @@ function allConsultation() {
         recordMetrics({ clicks: 2, drags: 2 });
     }
 }
+
+// Ecoute les instructions du script de fond au sujet de la popup
+const actions = {
+    'allConsultation': allConsultation,
+    'tpebis': () => sendLastTPEamount(),
+    'sendCustomAmount': (amount) => sendtpeinstruction(amount) // Ajout de l'action sendCustomAmount
+
+};
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action in actions) {
+        console.log(request.action + ' demandé');
+        if (request.action === 'sendCustomAmount' && request.amount !== undefined) {
+            let amount = request.amount;
+            // First the amount must contain only digits and exactly one or zero comma or dot
+            let amountCheck = amount.replace(/[^0-9,.]/g, '');
+            if (amountCheck.length !== amount.length) {
+                console.log('Amount', amount, 'is not valid');
+                console.warn('Amount', amount, 'is not valid');
+                return;
+            }
+            if (amount.match(/[,.]/g) && amount.match(/[,.]/g).length > 1) {
+                console.log('Amount', amount, 'is not valid');
+                console.warn('Amount', amount, 'is not valid');
+                return;
+            }
+            let splitedAmount = amount.split(/,|\./);
+            let amountUnits = splitedAmount[0];
+            let amountDecimals = splitedAmount[1] || '00';
+            if (amountDecimals.length === 1) {
+                amountDecimals += '0'; // Ajoute un zéro si la partie décimale a une seule position
+            } else if (amountDecimals.length > 2) {
+                amountDecimals = amountDecimals.slice(0, 2); // Coupe la partie décimale à 2 positions
+            }
+            amount = parseInt(amountUnits + amountDecimals, 10);
+            console.warn('[debug] pause');
+            actions[request.action](amount); // Appel avec le montant personnalisé
+        } else {
+            actions[request.action]();
+        }
+    }
+});
