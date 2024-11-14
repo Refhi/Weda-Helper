a692c101f6a7276c866be73c7c886ca8c8fc307d
Refhi <wedahelper@mailo.com>
2024-11-14 10:21:45 +0100

    changement d'un timeout

--- aati.js
+++ aati.js
@@ -185,7 +185,7 @@ addTweak(urlAATI, 'autoAATI', function () {
                             recordMetrics({ clicks: 3, drags: 3 });
                             setTimeout(function () {
                                 window.close();
-                            }, 10000); // essai avec un délai de 1s
+                            }, 1000); // essai avec un délai de 1s
                         })
                         .catch(error => {
                             console.warn(errortype + ' Impossible de joindre Weda-Helper-Companion : est-il bien paramétré et démarré ? Erreur:', error);
