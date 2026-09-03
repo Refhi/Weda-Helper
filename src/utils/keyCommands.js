95c77e8c2222ca82543faa1ee7ceff2a3c587217
Abel Ingrand <abel@ingrand.net>
2026-09-04 01:29:25 +0200

    Retrait d'un WarpButton qui entrait en conflit avec le waitForElement du pdfParser

--- src/utils/keyCommands.js
+++ src/utils/keyCommands.js
@@ -862,7 +862,6 @@ addTweak('*', 'WarpButtons', async function () {
         '#WHButtonValidAndDelete',
         '#ButtonValidFileStream',
         '#ContentPlaceHolder1_PatientsGrid_ButtonHistoriqueResultat_0', // Pour les biologies
-        '.btnImport.importPatient'
     ];
 
     selectors.forEach(selector => {
